026d0c2a0375c264f6163db99425fc04a2206645
Paolo Di Lorenzo <paolo@dilorenzo.org>
2025-05-25 23:19:33 -0400

    Replace revdepcheck with newer recheck workflow

--- .github/workflow-resources/release-checklist.R
+++
@@ -1,8 +0,0 @@
-release_checklist <- function(email, token, version) {
-    # Reverse dependency checks
-    revdepcheck::revdep_reset()
-    revdepcheck::revdep_check()
-
-    # Version number updates
-    desc::desc_set_version(version)
-}
